7177d3f3e98800e4786193675d993c071405224c
Nillander Alarcão <nillander@live.com>
2024-04-21 20:36:07 -0300

    View: Blade template engine

---
+++ resources/views/test.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Test</title>
+    </head>
+    <body>
+        <h1>Test</h1>
+        @php
+            $suco = 'orange juice';
+        @endphp
+        <p>This is a {{ $comida }} and {{ $suco }} page.</p>
+    </body>
+</html>

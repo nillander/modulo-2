70a88c52dd2f1e46b343206067543fef7a629998
Nillander Alarcão <nillander@live.com>
2024-04-25 15:52:30 -0300

    View: Organizando a visualização

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,18 +1,12 @@
-<html>
-
-<head>
-    <title>Test</title>
-</head>
-
-<body>
-    <h1>Test</h1>
+<x-app-layout>
+    <x-slot name='header'>
+        <h3>Test</h3>
+    </x-slot>
     @php
     $suco = 'orange juice';
     @endphp
     <p>This is a {{ $comida }} and {{ $suco }} page.</p>
-</body>
-<ul>
-    <li>Nome: {{ auth()->user()->name }} </li>
-</ul>
-
-</html>
+    <ul>
+        <li>Nome: {{ auth()->user()->name }} </li>
+    </ul>
+</x-app-layout>
